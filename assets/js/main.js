688ab237226130c0275b887a7b861bb5a638758a
Cesar Nobre <cesarnobrefilho@gmail.com>
2016-02-13 00:03:31 -0200

    finished first sandbox

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,8 @@ requirejs.config({
     paths: {
         'do-task': 'components/do-task',
         'text':'../../bower_components/text/text',
+        'knockout':'../../bower_components/knockout/dist/knockout.debug',
+        'Task':'components/dependencies/Task'
     }
 });
 
