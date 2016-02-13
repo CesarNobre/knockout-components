a42530821431900ba017839ab8c037c2bd3285b0
Cesar Nobre <cesarnobrefilho@gmail.com>
2016-02-13 00:02:56 -0200

    updated components

--- assets/js/components/do-task.js
+++ assets/js/components/do-task.js
@@ -1,3 +1,23 @@
-define(function (require){
-	debugger;
+define(['require','text!do-task.html','knockout','Task'],function (require, doTaskTemplate){
+	var ko = require('knockout');
+	var Task = require('Task');
+
+	function viewModel(){
+		var self = this;
+		self.task = ko.observable('');
+		self.tasks = ko.observableArray();
+
+		self.SaveTask = function(){
+			var task = new Task(self.task());
+			self.tasks.push(task);
+			self.task('');
+		}
+	}
+
+	ko.components.register('do-task', {
+    	viewModel: viewModel,
+    	template: doTaskTemplate
+	});
+
+	ko.applyBindings(new viewModel());
 });
