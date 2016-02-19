0a422716d32c745423300ebb84c51de205d1d333
cesarnobre <cesarnobrefilho@gmail.com>
2016-02-19 07:40:44 -0200

    implementation task done

--- assets/js/components/dependencies/Task.js
+++ assets/js/components/dependencies/Task.js
@@ -2,7 +2,19 @@ define(function (require){
 	var ko = require('knockout');
 
 	function Task(description){
-		this.Description = ko.observable(description);
+		var self = this;
+ +
+ +		self.description = ko.observable(description);
+ +		self.done = ko.observable();
+ +
+ +		self.TaskDone = function(){
+ +			var toggledBoolean = self.done(toggleBoolean(self.done()));
+ +			self.done(toggledBoolean);
+ +		}
+ +
+ +		var toggleBoolean = function(myBool){
+ +			myBool = !myBool;
+ +		}
 	}
 
 	return Task
