02b6b04b9aca59527f694a9660db7cf10ec15f8f
cesarnobre <cesarnobrefilho@gmail.com>
2016-02-25 07:42:19 -0300

    created localstorage wrapper

--- assets/js/storage.js
+++ assets/js/storage.js
@@ -1,3 +1,13 @@
-define([''], function(){
+define(['knockout'], function(ko){
+	var save = function(key, value){
+		localStorage.setItem(key, ko.toJSON(value));
+	}
+	var find = function(key){
+		return JSON.parse(localStorage.getItem(key));
+	}
 
+	return {
+		save : save,
+		find : find
+	}
 });
