5213eff1fa788a4983d6c8619815e9f2c38b3054
Cesar Nobre <cesarnobrefilho@gmail.com>
2016-03-06 14:20:20 -0300

    added more tests

--- assets/js/components/tests/do-task.spec.js
+++ assets/js/components/tests/do-task.spec.js
@@ -7,6 +7,12 @@ define(['do-task','Task'],function(doTask,Task){
 	    vm.SaveTask();
 	    expect(vm.tasks().length).toEqual(0);
 	  });
+	  it("if has some description, should save a task",function(){
+	  	var dummyElement = document.createElement('input');
+		document.getElementById = jasmine.createSpy('#typeTaskDescription').andReturn(dummyElement);
+	  	vm.task("some description");
+	  	vm.SaveTask();
+	  });
 	});
 });
 
